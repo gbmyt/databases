e73d35a3e52044957b28e9135b9b11f4f43264a0
gbmyt <gbmytabc1@gmail.com>
2022-11-16 23:28:48 -0500

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,13 +2,33 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  id INT NOT NULL AUTO_INCREMENT,
+  room_name TEXT,
+  PRIMARY KEY(id)
+);
 
+CREATE TABLE users (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  username TEXT
+);
 
+CREATE TABLE messages (
+  /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT,
+  msg TEXT,
+  user TEXT,
+  id_Users INT,
+  id_Rooms INT,
+  PRIMARY KEY(id),
+  FOREIGN KEY(id_Users) REFERENCES users(id),
+  FOREIGN KEY(id_Rooms) REFERENCES rooms(id)
+
+  -- FOREIGN KEY (SomeEntityID) REFERENCES SomeEntityTable(ID) ON DELETE CASCADE;
+
+);
 
 
 /*  Execute this file from the command line by typing:
